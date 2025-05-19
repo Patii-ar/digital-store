a7cbe816e03c9876c819839d886a9c3c8c915352
SudoBrunoWoodo <bruno1000oliveira@gmail.com>
2025-05-19 18:43:59 -0300

    Iniciando a estrutura css no header e footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,48 +2,52 @@ import logo from "../assets/logofooter.svg";
 import facebook from "../assets/logofacebook.svg";
 import instagram from "../assets/logoinstagram.svg";
 import x from "../assets/logox.svg";
+
 import { Link } from "react-router-dom";
+
+import "../css/Footer.css";
+
 export default function Footer () {
     return (
-        <footer>
-            <div>
-                <div>
+        <footer className="bg-(--darkgray) text-white flex flex-col w-full">
+            <div className="border-b flex justify-between principal">
+                <div className="basis-xs flex flex-col gap-9">
                     <img src={logo} alt="Logo Digital store"/>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident, reprehenderit, dolores, non amet eum nihil sint consectetur et maxime iste rerum nisi esse expedita omnis at ut deserunt. Officia, amet?</p>
-                    <div>
+                    <div className="flex gap-9">
                         <Link to="/"><img src={facebook} alt="Facebook" /></Link>
                         <Link to="/"><img src={instagram} alt="Instagram" /></Link>
                         <Link to="/"><img src={x} alt="X" /></Link>
                     </div>
                 </div>
                 <div>
-                    <h5>Informação</h5>
+                    <h5 className="font-semibold text-base">Informação</h5>
                     <ul>
-                        <li><Link to="/">Sobre Drip Store</Link></li>
-                        <li><Link to="/">Segurnça</Link></li>
-                        <li><Link to="/">Wishlist</Link></li>
-                        <li><Link to="/">Blog</Link></li>
-                        <li><Link to="/">Trabalhe Conosco</Link></li>
-                        <li><Link to="/">Meus Pedidos</Link></li>
+                        <li className="text-sm"><Link to="/">Sobre Drip Store</Link></li>
+                        <li className="text-sm"><Link to="/">Segurnça</Link></li>
+                        <li className="text-sm"><Link to="/">Wishlist</Link></li>
+                        <li className="text-sm"><Link to="/">Blog</Link></li>
+                        <li className="text-sm"><Link to="/">Trabalhe Conosco</Link></li>
+                        <li className="text-sm"><Link to="/">Meus Pedidos</Link></li>
                     </ul>
                 </div>
                 <div>
-                    <h5>Categorias</h5>
+                    <h5 className="font-semibold text-base">Categorias</h5>
                     <ul>
-                        <li><Link to="/">Camisetas</Link></li>
-                        <li><Link to="/">Calças</Link></li>
-                        <li><Link to="/">Bonés</Link></li>
-                        <li><Link to="/">Headphones</Link></li>
-                        <li><Link to="/">Tênis</Link></li>
+                        <li className="text-sm"><Link to="/">Camisetas</Link></li>
+                        <li className="text-sm"><Link to="/">Calças</Link></li>
+                        <li className="text-sm"><Link to="/">Bonés</Link></li>
+                        <li className="text-sm"><Link to="/">Headphones</Link></li>
+                        <li className="text-sm"><Link to="/">Tênis</Link></li>
                     </ul>
                 </div>
-                <div>
-                    <h5>Contato</h5>
-                    <p>Av. Santos Dumont, 1510 -1 andar - Aldeota, Fortaleza - CE, 60150-161</p>
+                <div className="contato basis-sm">
+                    <h5 className="font-semibold text-base">Contato</h5>
+                    <p>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</p>
                     <p>(85) 3051-3411</p>
                 </div>
             </div>
-            <h6>@ 2025 Digital College </h6>
+            <h6 className="text-center">@ 2025 Digital College </h6>
         </footer>
     );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,28 @@
 import { Link } from "react-router-dom";
+
 import logo from "../assets/logo.svg";
 import { CiSearch } from "react-icons/ci";
-import minicart from "../assets/mini-cart.svg"
+import minicart from "../assets/mini-cart.svg";
 
-
+import "../css/Header.css";
 
 export default function Header () {
     return (
-        <header>
-            <section>
+        <header className="flex flex-col">
+            <section className="primeira-section flex justify-between">
                 <Link to="/"><img src={logo} alt="Logo Digital Store" /></Link>
-                <div>
-                    <input type="text" placeholder="Pesquisar Produto..."></input>
-                    <button><CiSearch/></button>
+                <div className="searchbar bg-(--lightgray3) flex justify-between rounded-lg">
+                    <input className="input-pesquisa w-7/8" type="text" placeholder="Pesquisar Produto..."></input>
+                    <button className="botao-pesquisa cursor-pointer"><CiSearch /></button>
                 </div>
-                <Link to="/"><h4>Cadastre-se</h4></Link>
-                <Link to="/"><button>Entrar</button></Link>
-                <Link to="/"><img src={minicart} alt="Carrinho" /></Link>
+                <div className="areacadastro flex gap-10">
+                    <Link to="/"><h4 className="font-normal underline text-center text-(--darkgray2) cursor-pointer">Cadastre-se</h4></Link>
+                    <Link to="/"><button className="botatoentrar rounded-lg bg-(--principal) font-bold text-white cursor-pointer">Entrar</button></Link>
+                </div>
+                <Link to="/"><img src={minicart} alt="Carrinho" className="carrinho"/></Link>
             </section>
             <nav>
-                <ul>
+                <ul className="lista flex text-(--darkgray2)">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/produtos">Produtos</Link></li>
                     <li><Link to="/">Categorias</Link></li>
